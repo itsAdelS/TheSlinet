67699c33662ccf558b528dca169dcebef094a527
Adel Sliman <115944408+itsAdelS@users.noreply.github.com>
2024-03-03 16:45:15 -0500

    fixed collection bug

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,54 +1,129 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useSearchParams, useRouter } from "next/navigation";
+import { CldImage } from "next-cloudinary";
 
-import { Input } from "@/components/ui/input";
-import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { transformationTypes } from "@/constants";
+import { IImage } from "@/lib/database/models/image.model";
+import { formUrlQuery } from "@/lib/utils";
 
-export const Search = () => {
+import { Button } from "../ui/button";
+
+import { Search } from "./Search";
+
+export const Collection = ({
+  hasSearch = false,
+  images,
+  totalPages = 1,
+  page,
+}: {
+  images: IImage[];
+  totalPages?: number;
+  page: number;
+  hasSearch?: boolean;
+}) => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const [query, setQuery] = useState("");
 
-  useEffect(() => {
-    const delayDebounceFn = setTimeout(() => {
-      if (query) {
-        const newUrl = formUrlQuery({
-          searchParams: searchParams.toString(),
-          key: "query",
-          value: query,
-        });
+  // PAGINATION HANDLER
+  const onPageChange = (action: string) => {
+    const pageValue = action === "next" ? Number(page) + 1 : Number(page) - 1;
 
-        router.push(newUrl, { scroll: false });
-      } else {
-        const newUrl = removeKeysFromQuery({
-          searchParams: searchParams.toString(),
-          keysToRemove: ["query"],
-        });
+    const newUrl = formUrlQuery({
+      searchParams: searchParams.toString(),
+      key: "page",
+      value: pageValue,
+    });
 
-        router.push(newUrl, { scroll: false });
-      }
-    }, 300);
-
-    return () => clearTimeout(delayDebounceFn);
-  }, [router, searchParams, query]);
+    router.push(newUrl, { scroll: false });
+  };
 
   return (
-    <div className="search">
-      <Image
-        src="/assets/icons/search.svg"
-        alt="search"
-        width={24}
-        height={24}
-      />
+    <>
+      <div className="collection-heading">
+        <h2 className="h2-bold text-dark-600">Recent Edits</h2>
+        {hasSearch && <Search />}
+      </div>
 
-      <Input
-        className="search-field"
-        placeholder="Search"
-        onChange={(e) => setQuery(e.target.value)}
-      />
-    </div>
+      {images.length > 0 ? (
+        <ul className="collection-list">
+          {images.map((image) => (
+            <Card image={image} key={image._id} />
+          ))}
+        </ul>
+      ) : (
+        <div className="collection-empty">
+          <p className="p-20-semibold">Empty List</p>
+        </div>
+      )}
+
+      {totalPages > 1 && (
+        <Pagination className="mt-10">
+          <PaginationContent className="flex w-full">
+            <Button
+              disabled={Number(page) <= 1}
+              className="collection-btn"
+              onClick={() => onPageChange("prev")}
+            >
+              <PaginationPrevious className="hover:bg-transparent hover:text-white" />
+            </Button>
+
+            <p className="flex-center p-16-medium w-fit flex-1">
+              {page} / {totalPages}
+            </p>
+
+            <Button
+              className="button w-32 bg-purple-gradient bg-cover text-white"
+              onClick={() => onPageChange("next")}
+              disabled={Number(page) >= totalPages}
+            >
+              <PaginationNext className="hover:bg-transparent hover:text-white" />
+            </Button>
+          </PaginationContent>
+        </Pagination>
+      )}
+    </>
+  );
+};
+
+const Card = ({ image }: { image: IImage }) => {
+  return (
+    <li>
+      <Link href={`/transformations/${image._id}`} className="collection-card">
+        <CldImage
+          src={image.publicId}
+          alt={image.title}
+          width={image.width}
+          height={image.height}
+          {...image.config}
+          loading="lazy"
+          className="h-52 w-full rounded-[10px] object-cover"
+          sizes="(max-width: 767px) 100vw, (max-width: 1279px) 50vw, 33vw"
+        />
+        <div className="flex-between">
+          <p className="p-20-semibold mr-3 line-clamp-1 text-dark-600">
+            {image.title}
+          </p>
+          <Image
+            src={`/assets/icons/${
+              transformationTypes[
+                image.transformationType as TransformationTypeKey
+              ].icon
+            }`}
+            alt={image.title}
+            width={24}
+            height={24}
+          />
+        </div>
+      </Link>
+    </li>
   );
 };
